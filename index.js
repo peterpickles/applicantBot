7f4e566c387a029c3fb5be8b74ed68acea4b0de7
peterpickles <lipeter2006@gmail.com>
2018-02-21 13:46:08 -0800

    getting auth working and basic structure setup

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-var twit = require("twit");
-
-var T = new Twit({
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
-  })
+console.log('It is rising!');
+var Twit = require("twit");
+var config = require('./config');
+var T = new Twit(config);
