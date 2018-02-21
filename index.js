069140796b44377c2de35eea348e697682d95d4e
peterpickles <lipeter2006@gmail.com>
2018-02-21 15:35:25 -0800

    got searching to work, looking at working on the stream api

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ console.log('It is rising!');
 var Twit = require("twit");
 var config = require('./config');
 var T = new Twit(config);
+
+//get requests - search by hashtag, location, user
+//post request - Tweeting about thing
+//stream --contineous user @mention  use that and get back
+var params = { 
+    q: 'rainbow', 
+    count: 2
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+    var tweets = data.statuses;
+    for (var i = 0; i <tweets.length; i++) {
+        console.log(tweets[i].text);
+    }
+}; 
