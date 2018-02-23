93e0c1c11b1469ba0d167826f957e65b43bc610a
peterpickles <lipeter2006@gmail.com>
2018-02-23 04:38:00 -0800

    separated the questions and answers from the main file

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-console.log('It is rising!');
+console.log('Bot starting!');
 var Twit = require("twit");
-var config = require('./config');
+var config = require('./config.js');
 var T = new Twit(config);
+var stream = T.stream('user');
+var question = require('./question.js');
 
-//get requests - search by hashtag, location, user
-//post request - Tweeting about thing
-//stream --contineous user @mention  use that and get back
-var params = { 
-    q: 'rainbow', 
-    count: 2
+
+
+function tweetIt(txt){
+    var tweet = {
+        status: txt
+    }
+    T.post('statuses/update', tweet, tweeted);
+    function tweeted(err, data, response) {
+        if (err){
+            console.log("Something went wrong", err);
+        } else {
+            console.log("It worked!");
+        }
+    }
 }
 
-T.get('search/tweets', params, gotData);
 
-function gotData(err, data, response) {
-    var tweets = data.statuses;
-    for (var i = 0; i <tweets.length; i++) {
-        console.log(tweets[i].text);
-    }
-}; 
+stream.on('tweet', tweetEvent);
+function tweetEvent(tweet) {
+    var replyto = tweet.in_reply_to_screen_name;
+    var from = tweet.user.screen_name;
+    var text = tweet.text;
+    console.log("Tweet Object ", tweet);
+    questions.forEach(function(question, index){
+        if(text.includes(question)){
+            tweetIt('@'+ from + answers[index]);
+        }
+        
+    })
+        
+    //while someone is tweeting at me
+    //if they say "What's the weather today?"
+    //I will say "rainy!"
+}
+
+
+stream.on('follow', followed);
+function followed(tweet) {
+    var name = tweet.source.name;
+    var screenName = tweet.source.screen_name;
+    console.log("the screename is ", screenName);
+    tweetIt('@' + screenName + ' How you doinnnnn!');
+}
+
+
+
+ 
---
+++ question.js
@@ -0,0 +1,12 @@
+var questions = [
+    "what's my trouser color?",
+    "What makes you special?",
+    "Tell me about yourself",
+    "Tell me about a time where you worked with a difficult person and how you dealt with it?"
+]
+var answers = [
+    "red!",
+    "If would describe my", 
+    "I'm a red power ranger and can call upon a megazord to destroy my enemies",
+    "One time we worked "
+]
