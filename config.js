11c677d6afeb450b31d124aeb13c12685f7c6bd2
peterpickles <lipeter2006@gmail.com>
2018-02-23 09:34:16 -0800

    getting config file in

---
+++ config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//     consumer_key:         'U7n8aAuA1yyqPic7IjHIQezyr',
+//     consumer_secret:      'Sl0RZaf82zgLXLC2kvRKaLcsnqSyOq5zwTorn68Kzumwy9LwMe',
+//     access_token:         '906949183155793922-XKtDiRIi87wMNbtcIGJyax6HV0jXXaU',
+//     access_token_secret:  'E7ARr3ECYlM8DmMCjkYRCeFGgTpQtl6I8C16Qq3hS2aef',
+// }
+module.exports = {
+    consumer_key:         'ocEe2tJxymZeiYG98TPxMmu9N',
+    consumer_secret:      'qbaIPXty7NwkL5QB88uH9PFdhXBqJQ9Je3AE46dTgE9qLaFRFy',
+    access_token:         '365232802-HcrX8gGb2Z6JV9vCUc11O8e6intlEuNoV79hplIS',
+    access_token_secret:  'HMWYVTTbJPLhGtU4svV228WaWau2Dzycplfv16Uc84wdD',
+}
